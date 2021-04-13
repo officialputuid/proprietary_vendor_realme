f74edfbf8f4030fd15df6d46822a6e6a15ad6678
SamarV-121 <samarvispute121@gmail.com>
2021-04-13 09:30:28 +0000

    Initial commit
    Change-Id: Ib1d6becb193ab19fef6882ff3a719d9703b27d60
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

---
+++ RMX2001/RMX2001-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libdpframework.so \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib/libvpud_vcodec.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libvpud_vcodec.so \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib64/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libdpframework.so
+PRODUCT_COPY_FILES += \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libdpframework.so \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib/libvpud_vcodec.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libvpud_vcodec.so \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib64/libcameracustom.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libcameracustom.so \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib64/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libdpframework.so
