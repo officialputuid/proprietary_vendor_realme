b25bcf74e7c6e53a2806c5b935195505a1bb2621
officialputuid <officialputuid@hack.id>
2021-07-28 18:07:20 +0800

    RMX2001: Added RealmeDirac files necessary - Fix ReameDirac Crash
    Co-Authored-By: Sonal Singh <sonal.singh.19993@gmail.com>
    Co-authored-by: techyminati <sinha.aryan03@gmail.com>
    Co-authored-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- RMX2001/RMX2001-vendor.mk
+++ RMX2001/RMX2001-vendor.mk
@@ -5,6 +5,13 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/realme/RMX2001
 
+# Dirac
+PRODUCT_COPY_FILES += \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/etc/audio_effects.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/audio_effects.xml \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/etc/diracmobile.config:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/diracmobile.config \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libDiracAPI_SHARED.so \
+    vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/soundfx/libdirac.so
+
 PRODUCT_COPY_FILES += \
     vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libdpframework.so \
     vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib/libvpud_vcodec.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libvpud_vcodec.so \
